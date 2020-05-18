1e50e4b86791553b13f97b5fa954b13a92f3b83b
Dennis Padiernos <depadiernos@gmail.com>
2020-05-18 17:19:37 -0500

    console log issue object inside payload.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ async function main() {
   try {
     const url = core.getInput('slack-url')
     const payload = github.context.payload
-    const message = `A new issue was created.\n(${payload.title})[${payload.url}]`
-    console.log(payload)
+    const message = `A new issue was created.\n(${payload.issue.title})[${payload.issue.url}]`
+    console.log(payload.issue)
     await axios.post(url, {
       text: {
         type: 'mrkdown',
