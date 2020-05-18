41934067a400ec998484fb5c8a15ae7cdf2ada39
Dennis Padiernos <depadiernos@gmail.com>
2020-05-18 15:37:55 -0500

    make payload specific.

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   const notify = async () => {
     await axios.post(url, {
-      text: payload,
+      text: {
+        type: 'mrkdown',
+        text: `A new issue was created.\n(${payload.title})[${payload.url}]`
+      },
     })
   }
   notify()
