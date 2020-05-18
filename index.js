75047269325e2c62c3cd4e19353d6befce754fda
Dennis Padiernos <depadiernos@gmail.com>
2020-05-18 16:27:33 -0500

    correct the function to make sure the promise is handled correctly.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 const axios = require('axios')
-try {
-  const url = core.getInput('slack-url')
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  const notify = async () => {
+
+const main = async () => {
+  try {
+    const url = core.getInput('slack-url')
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
     await axios.post(url, {
       text: {
         type: 'mrkdown',
-        text: `A new issue was created.\n(${payload.title})[${payload.url}]`
+        text: `A new issue was created.\n(${payload.title})[${payload.url}]`,
       },
     })
+  } catch (error) {
+    core.setFailed(error.message)
   }
-  notify()
-} catch (error) {
-  core.setFailed(error.message)
 }
