b168cd103e54fab0c42416687b6c46145f90e36f
Dennis Padiernos <depadiernos@gmail.com>
2020-05-18 20:03:56 -0500

    simplify payload

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ async function main() {
   try {
     const url = core.getInput('slack-url')
     const payload = github.context.payload
-    const message = `A new *issue* was created. -- [${payload.issue.title}](${payload.issue.url})`
+    const message = `A new *issue* was created. -- Title: ${payload.issue.title}\nUrl: ${payload.issue.url}`
 
     const res = await axios.post(url, {
-      type: "mrkdown",
       text: message
     })
     console.log(res)
